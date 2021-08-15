75e96f54c0c4c551a6b24b8b01195b7e5e678533
jbmakib <mdfoujolmobin@gmail.com>
2021-08-15 15:26:43 +0600

    deposit updated added

--- dashboard/banking.js
+++ dashboard/banking.js
@@ -2,12 +2,39 @@ let lastLoggedInData = JSON.parse(localStorage.getItem("last-logged-in"));
 console.log(lastLoggedInData)
 if (lastLoggedInData.status == false) {
     window.location.href = "../";
-}
+};
+
+// check the name of user
+document.getElementById("fullName-dash").innerText = lastLoggedInData.user;
+const userPass = lastLoggedInData.password;
+
 
 // deposit
-document.getElementById('deposit-button').addEventListener("click", function () {
+document.getElementById('deposit-button').addEventListener("click", function (e) {
+    e.preventDefault(); // stop loading
+
+    // get the pass
+    const inpPass = document.getElementById("deposit-password");
     // get the amount deposited
     const depositInput = document.getElementById('deposit-input');
+
+    // return if the password or ammount is blank
+    if (depositInput.value == "" && inpPass.value == "") {
+        window.alert("Deposit Amount and Password can't be blank");
+        return;
+    } else if (depositInput.value == "") {
+        window.alert("Please Enter Valid Deposit Amount");
+        return;
+    } else if (inpPass.value == "") {
+        window.alert("Please Enter Valid Deposit Amount");
+        return;
+    }
+
+    // return if the password not matched
+    if (inpPass.value !== userPass) {
+        window.alert("password not matched");
+        return;
+    }
     const newDepositAmount = parseFloat(depositInput.value);
 
 
@@ -30,8 +57,32 @@ document.getElementById('deposit-button').addEventListener("click", function () 
 });
 
 // withdraw
-document.getElementById("withdraw-button").addEventListener("click", function () {
+document.getElementById("withdraw-button").addEventListener("click", function (e) {
+    e.preventDefault();
+
+
+    // get the pass
+    const inpPass = document.getElementById("deposit-password");
+    // get the amount withdrawed
     const withdrawInput = document.getElementById("withdraw-input");
+
+    // return if the password or ammount is blank
+    if (depositInput.value == "" && inpPass.value == "") {
+        window.alert("Deposit Amount and Password can't be blank");
+        return;
+    } else if (depositInput.value == "") {
+        window.alert("Please Enter Valid Deposit Amount");
+        return;
+    } else if (inpPass.value == "") {
+        window.alert("Please Enter Valid Deposit Amount");
+        return;
+    }
+
+    // return if the password not matched
+    if (inpPass.value !== userPass) {
+        window.alert("password not matched");
+        return;
+    }
     const newWithdrawAmount = parseFloat(withdrawInput.value);
 
     // set withdraw total
@@ -53,11 +104,8 @@ document.getElementById("withdraw-button").addEventListener("click", function ()
     withdrawInput.value = '';
 });
 
-function welcome(username, password, fullname) {
-    document.getElementById("fullName-dash").innerText = fullname;
-}
 
 function logout() {
     localStorage.setItem("last-logged-in", JSON.stringify({ user: "", userID: "", password: "", status: false }));
     window.location.href = "../";
-}
+};
